4cee1a6836a5532ad7a3d8c0d04c5adf159c064b
Songoi Stephen <stewason@gmail.com>
2018-08-19 23:49:24 +0300

    [Finishes #159805560]Handle user verification

--- stack-app/app/user/user_model.py
+++ stack-app/app/user/user_model.py
@@ -1,15 +1,16 @@
 class User(object):
-    def __init__(self, username, usermail, userpassword, birthday):
-        self.username = username
-        self.usermail = usermail
-        self.birthday = birthday
-        self.userpassword = userpassword
+    def __init__(self):
         self.users_list = []
 
     def get_users(self):
         return self.users_list
 
     def register_user(self, username, usermail, birthday, userpassword):
+        self.username = username
+        self.usermail = usermail
+        self.birthday = birthday
+        self.userpassword = userpassword
+
         id = len(self.users_list) + 1
         new_user = {
             "mail" : usermail,
@@ -19,12 +20,13 @@ class User(object):
             "password" : userpassword
 
         }
-        return self.users_list.append(new_user)
+        self.users_list.append(new_user)
+        return new_user
 
-    def login(self, usermail, userpassword, username=None):
+    def login(self, usermail, userpassword):
         for user in self.users_list:
-            if user["name"] or user["userpassword"] in self.users_list:
-                if user["password"] == userpassword and user["mail"] == usermail:
-                    print ("login successfull")
+            if user["mail"] == usermail:
+                if user["password"] == userpassword:
+                    return True
                 else:
-                    print("authorizeation denied")
+                    return False
