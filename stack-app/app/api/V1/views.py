eab321335e80e4dc09c55ce9f8cb2eeb70e7f83f
Songoi Stephen <stewason@gmail.com>
2018-08-19 08:06:30 +0300

    [Fixes #159803987] Restructure directory structure

---
+++ stack-app/app/api/V1/views.py
@@ -0,0 +1,26 @@
+from flask import make_response, request, jsonify
+
+from flask_api import FlaskAPI
+
+# from app.user import User
+
+app = FlaskAPI(__name__)
+
+
+@app.route('/api/v1/users', methods=["GET"])
+def home():
+    return "this worked"
+# def list_users():
+#      """
+#        Instantiate the User Class
+#      """
+#      userMaker = User(""songoi", "songoi@example.com", "password", "birthday")
+#      """
+#       Register the user so that the users list inside the User model can have users
+#      """   
+#      userMaker.register_user("kim", "kim@example.com", "birthday")
+#      """
+#      Get the users now through the class method
+#      """
+#      users = userMaker.get_users()
+#      return make_response(jsonify( { "users" : users } ))
---
+++ stack-app/run.py
@@ -0,0 +1,5 @@
+from app.api.V1 import views
+from app.api.V1.views import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
