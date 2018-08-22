413b080b3b5a4aca0c926d07b062ab7ada53d819
Songoi Stephen <stewason@gmail.com>
2018-08-22 08:45:58 +0300

    Change user interface on dashboard

--- UI/scripts/design.js
+++ UI/scripts/design.js
@@ -1,6 +1,3 @@
-// var hiddenContent = document.getElementsByClassName("collapser");
-// var block = document.getElementsByClassName("collapse_content");
-
 function showContent (element){
     var block = element.nextElementSibling
     if (block.style.display === "block") {
@@ -10,21 +7,3 @@ function showContent (element){
     }
 };
     
-
-// hiddenContent.addEventListener("click", showContent);
-
-
-// var coll = document.getElementsByClassName("collapser");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
