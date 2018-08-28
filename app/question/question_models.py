024528858c8255ae0574532540cee42357578a9c
Songoi Stephen <stewason@gmail.com>
2018-08-28 12:13:03 +0300

    Handle question logic

--- app/question/question_models.py
+++ app/question/question_models.py
@@ -18,6 +18,7 @@ class Question(object):
         return question_list
 
     def get_question_by_id(self, question_id):
+<<<<<<< HEAD
         for item in question_list:
             if item["post_id"] == question_id:
 <<<<<<< HEAD
@@ -29,6 +30,12 @@ class Question(object):
                 return item
             return "No question with id {}".format(str(question_id))
 >>>>>>> feedback implementation
+=======
+        question =  [item for item in question_list if item["post_id"] == question_id]
+        if len(question) == 0:
+            return "No question with id {}".format(str(question_id))
+        return question
+>>>>>>> Handle question logic
 
 
     def create_question(self, the_question):
@@ -46,6 +53,7 @@ class Question(object):
         
         question_list.append(new_question)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         return question_list
         # self.get_question_byID(id)
@@ -75,15 +83,20 @@ class Check_user_input(object):
 
 =======
         return self.get_question_byID(id)
+=======
+        return self.get_question_by_id(id)
+>>>>>>> Handle question logic
 
-    def del_question(self, question_id):
-        print(len(question_list))
-
-        for item in question_list:
-            if item["post_id"] == question_id: 
-                question_list.remove(item)
-
-            return question_list
+    def del_question(self, question_id):      
+        if question_id >len(question_list) or question_id <= 0:
+            return "No question with id {}".format(str(question_id))
+                
+        else: 
+            for item in question_list:
+                if item["post_id"] == question_id: 
+                    question_list.remove(item)
+                return question_list
+        
 
     
 >>>>>>> feedback implementation
