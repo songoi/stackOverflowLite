89e9115a0bc15db00c5f61a2fe9cc2e204645980
Songoi Stephen <stewason@gmail.com>
2018-08-28 16:32:46 +0300

    Endpoints and their tests

--- app/api/V1/views.py
+++ app/api/V1/views.py
@@ -10,23 +10,25 @@ myapp = FlaskAPI(__name__)
 
 
 @myapp.route('/api/v1/questions', methods = ["GET", "POST"])
-def list_all_questions():
+def list_questions(the_question = None):
     questions_object = Question()
 
     if request.method == 'GET':
-        all_questions= questions_object.get_questions()
+        all_questions = questions_object.get_questions()
+       
         return make_response(jsonify( { "questions": all_questions} ))
 
     if request.method == 'POST':
         req_data = request.get_json()
-        new_question = req_data['your_question']
-
+           
+        new_question = req_data.get('question')
+        
         questions_object.create_question(new_question)
     
         return make_response(jsonify({"questions": questions_object.get_questions()}))
 
 @myapp.route('/api/v1/questions/<int:question_id>', methods = ['GET', 'POST', 'DELETE'])
-def get_question(question_id):
+def get_question(question_id = None):
     questions_object = Question()
 
     if request.method == 'GET':
@@ -34,14 +36,14 @@ def get_question(question_id):
         return make_response(jsonify( { "question": querried_question} ))
     
     if request.method == 'DELETE':      
-        return make_response(jsonify( { "questions": questions_object.del_question("question_id")} ))
+        return make_response(jsonify( { "questions": questions_object.del_question(question_id)} ))
 
 
 
 @myapp.route('/api/v1/questions/<int:question_id>/answer', methods = ['POST'])
-def post_answer(question_id):
-
+def post_answer(question_id = None, answer):
+    answer_object = Answer()
     req_data = get_json()
     new_answer = request.data.get("your_answer")
-    return make_response(jsonify( { "question":  questions_object.answer(new_answer,question_id)} ))
+    return make_response(jsonify( { "question":  answer_object.post_answer(new_answer,question_id)} ))
     
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,22 +7,27 @@ from app.api.V1.views import myapp
 
 
 
-class TestUserEndpoints(unittest.TestCase):   
+class BaseTest(unittest.TestCase):   
     user_endpoint = '/api/v1/questions'
     myapp = myapp.test_client()
+    new_question = "question example 1"
+    new_answer = "answer example 1"
 
-    def test_create_user(self):
-        response = self.myapp.post(self.user_endpoint)
+class TestQuetion(BaseTest):
+    
+    def test_post_question(self):
+        response = myapp.post(user_endpoint, content_type='application/json', 
+            data=json.dumps(dict(question = new_question))
         self.assertEqual(response.status_code, 200)
-        
+        self.assertIn(respose, "question example 1" )
+ 
         
     def test_list_all_questions(self):
-        response = self.myapp.get(self.user_endpoint)
+        response = myapp.get(self.user_endpoint)
         self.assertEquals(response.status_code, 200)
+        assertIn(response)
     
-        response = self.myapp.post(self.user_endpoint)
-        self.assertEquals(response.status_code, 200)
-    
+
     def test_get_question(self):
         response = self.myapp.get(self.user_endpoint+ '/1')
         self.assertEquals(response.status_code, 200)
@@ -33,6 +38,11 @@ class TestUserEndpoints(unittest.TestCase):
         response = self.myapp.delete(self.user_endpoint+ '/1')
         self.assertEquals(response.status_code, 200)
     
-
+    
+    def test_create_user(self):
+        response = self.myapp.post(self.user_endpoint)
+        self.assertEqual(response.status_code, 200)
+        
+    
 if __name__ == '__main__':
     unittest.main()
